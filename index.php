d43486e12747fc58b1941d4b91595b25f7aa2853
Thomas Nguyen <thomasnguyen@fas.harvard.edu>
2015-07-26 12:41:01 -0400

    Batch upload

--- index.php
+++ index.php
@@ -1,31 +1,48 @@
 <!DOCTYPE html>
+
 <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Skeleton: Responsive CSS Boilerplate</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='//fonts.googleapis.com/css?family=Raleway:400,300,600' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/Skeleton/css/normalize.css">
+    <link rel="stylesheet" href="css/Skeleton/css/skeleton.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/animate.css">
+  </head>
 
-<head>
-  <meta charset="utf-8">
-  <title>Skeleton: Responsive CSS Boilerplate</title>
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  
-  <link href='//fonts.googleapis.com/css?family=Raleway:400,300,600' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" href="Skeleton/css/normalize.css">
-  <link rel="stylesheet" href="Skeleton/css/skeleton.css">
-  
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-</head>
-
-<body>
-  <div class="section hero">
-    <div class="container">
-      <h1>Thomas Nguyen</h1>
-      <p>Using Skeleton and Animiate CSS</p>
+  <body>
+    <div class="hero center">
+      <div class="container">
+        <h1 class="fadeInDown">Thomas Nguyen</h1>
+        <p>Using Skeleton and Animiate CSS</p>
+      </div>
     </div>
-  </div>
+    <div class="container">
+      <h5>Hello Everybody!</h5>
+      <div class="row">
+        <div class="six columns">
+          <p>My name is Thomas Nguyen. As you may already know, you are on my personal website. I am trying something new with this rebuild of my site.</p>
+          <ul>
+            <li>Moving from Bootstrap to Skeleton</li>
+            <li>Incorporating Animiate CSS</li>
+            <li>Other jQuery and JavaScript stuff</li>
+          </ul>
+          <p>For those that know me, you can follow my progress on GitHub. Thanks for visiting!</p>
+        </div>
+        <div class="six columns">
+          <img src="/images/tnguyen.jpg" alt="Thomas Nguyen" class="center-img">
+        </div>
+      </div><!-- ./row -->
+    </div><!-- ./container -->
 
-  <div class="container">
-    <p>This is body content</p>
-  </div>
-</body>
-
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+    <script>
+      $('h1').addClass('animated bounceOutLeft');
+    </script>
+  </body>
 </html>
