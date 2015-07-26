d1b1f69e1bb469a217ce419a119b98653f9373b8
Thomas Nguyen <thomasnguyen@fas.harvard.edu>
2015-07-26 11:10:02 -0400

    Added content and css files

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
-hello, world.
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Skeleton: Responsive CSS Boilerplate</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link href='//fonts.googleapis.com/css?family=Raleway:400,300,600' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="Skeleton/css/normalize.css">
+  <link rel="stylesheet" href="Skeleton/css/skeleton.css">
+  
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+</head>
+
+<body>
+  <div class="section hero">
+    <div class="container">
+      <h1>Thomas Nguyen</h1>
+      <p>Using Skeleton and Animiate CSS</p>
+    </div>
+  </div>
+
+  <div class="container">
+    <p>This is body content</p>
+  </div>
+</body>
+
+</html>
