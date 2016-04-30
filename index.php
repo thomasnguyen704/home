b7d7bbae356aa057a764a6e3147168556693e57a
Thomas Nguyen <thomasnguyen@fas.harvard.edu>
2016-04-30 17:03:06 -0400

    changed banner text size

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
                 <div class="row">
                     <div class="col-sm-5">
                         <div class="well">
-                            <h2><span class="text-red">"What is it like being an Asian American?"</span> I try to answer this question by reflecting on my life and those lives around me.</h2>
+                            <h1><span class="text-red">"What is it like being an Asian American?"</span> I try to answer this question by reflecting on my life and those lives around me.</h1>
                         </div>
                     </div>
                 </div>
