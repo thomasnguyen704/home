52763dd313c2c12e609f3aba0f08024bab186ea0
Thomas Nguyen <thomasnguyen@fas.harvard.edu>
2016-05-07 08:58:38 -0400

    navbar

--- index.php
+++ index.php
@@ -28,9 +28,7 @@
         </div>
         <nav>
             <ul class="nav nav-pills container">
-                <li role="presentation"><a href="http://thomasnguyen.me/">Home</a></li>
-                <li role="presentation"><a href="#">About</a></li>
-                <li role="presentation"><a href="https://www.linkedin.com/in/tomnguyen704" target="_blank">Contact</a></li>
+                <li role="presentation" class="pull-right"><a href="https://www.linkedin.com/in/tomnguyen704" target="_blank">Contact</a></li>
             </ul>
         </nav>
 
